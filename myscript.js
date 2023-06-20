17d8691db6a5f3db2b3b694cd622657a318746cc
Binaya077 <binayapokhrel70@gmail.com>
2023-06-20 17:06:45 +0545

    responsive navbar

---
+++ myscript.js
@@ -0,0 +1,5 @@
+const tripledots = document.getElementById('tripledot');
+const navbar = document.querySelector('#navbar');
+tripledots.addEventListener("click", () => {
+    navbar.classList.toggle('active')
+} )
